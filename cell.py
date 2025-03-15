576e797a7651a29b01087a7e7a48d745fde4a683
Quella <132938295+qquella@users.noreply.github.com>
2025-03-15 15:27:37 -0400

    add the draw move method draw a line from the center of one cell to another and show different color to backtrack when we draw new line

--- cell.py
+++ cell.py
@@ -19,6 +19,9 @@ class Cell:
         self._win = win
 
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
+
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
@@ -36,3 +39,19 @@ class Cell:
         if self.has_bottom_wall:
             l4 = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(l4)
+
+    def draw_move(self, to_cell, undo=False):
+        half_len1 = abs(self._x1 - self._x2) // 2
+        center_x1 = half_len1 + self._x1
+        center_y1 = half_len1 + self._y1
+
+        half_len2 = abs(to_cell._x1 - to_cell._x2) // 2
+        center_x2 = half_len2 + to_cell._x1
+        center_y2 = half_len2 + to_cell._y1
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        line = Line(Point(center_x1, center_y1), Point(center_x2, center_y2))
+        self._win.draw_line(line, fill_color)
